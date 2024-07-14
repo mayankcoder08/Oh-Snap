921c9af19426e1001c0b9cadd3ccbb8cba5a4ffa
Mayank Shrivastava <mayankshivastava802@gmail.com>
2024-07-14 15:34:27 +0530

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -36,7 +37,9 @@ function App() {
     <div className="min-h-screen flex-wrap content-between bg-gray-500">
       <div className="w-full block">
         <Header />
-        <main></main>
+        <main>
+          <Outlet></Outlet>
+        </main>
         <Footer />
       </div>
     </div>
