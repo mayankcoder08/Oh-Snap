22f0d2754ea2c9426d51ce91a400b8ea38e3b99c
Mayank Shrivastava <mayankshivastava802@gmail.com>
2024-07-14 14:32:49 +0530

    slug transform

---
+++ src/components/RTE.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {Editor} from "@tinymce/tinymce-react"
+import { Controller } from "react-hook-form";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "Content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+                initialValue:defaultValue,
+              branding: 500,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic forecolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      ></Controller>
+    </div>
+  );
+}
+
+// function RTE() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default RTE
