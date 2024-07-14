41ed7f00cd9a0e78d2ba0f8b6239dda0d17c6628
Mayank Shrivastava <mayankshivastava802@gmail.com>
2024-07-14 14:59:02 +0530

    Adding Pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,11 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 
 export {
     Header,
@@ -12,4 +17,9 @@ export {
     Logo,
     LogoutBtn,
     RTE,
+    Signup,
+    Login, 
+    PostForm,
+    PostCard,
+    AuthLayout
 }
